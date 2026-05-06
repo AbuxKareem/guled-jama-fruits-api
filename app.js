5e5a0467de496c71ea7028831bbb0292a2d72ae8
AbuxKareem <akareembusiness@gmail.com>
2026-05-06 12:38:04 +0100

    new changes made

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // instructions to run the program
 
 const express = require("express")
+const cors = require('cors')
 const app = express()
 const fruits = require("./routes/fruits")
 
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
 //when we dont pass a route, this function will run on all routes
 // converts all html bodies sent into json
 // order of these statements matters!
+app.use(cors())
 app.use(express.json())
 //if you hit any http request with the word 'fruits' in it, point those to the router
 // useful for multiple roots ! i.e. /fruits/1 and fruits/id=999 etc
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require('dotenv').config()
+const port = process.env.port
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
-app.use('/fruits', require('./routes/fruits'))
 
 app.listen(port, () => {
     console.log(`Simple api is listening on port ${port}`)
